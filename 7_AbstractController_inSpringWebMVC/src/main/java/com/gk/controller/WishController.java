6a46f7294a2d261b24f6c968004f8de206aaace7
Pranay Singh <pranay.singh.1393@gmail.com>
2020-11-26 00:30:26 +0530

    This Controller Classe will be used to prepare Controller classes where no form data submission in request, but, it will dispay dynamic content through web pages.
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ 7_AbstractController_inSpringWebMVC/src/main/java/com/gk/controller/WishController.java
@@ -0,0 +1,32 @@
+package com.gk.controller;
+
+import java.time.LocalTime;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/*
+ * Note: In this, no form submission is going on, but, 
+ * dynamic content will be included in response.
+ */
+public class WishController extends AbstractController {
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		LocalTime time = LocalTime.now();
+		int hour = time.getHour();
+		String wish_Message = "";
+		if (hour < 12) {
+			wish_Message = "Good Morning";
+		} else if (hour < 17) {
+			wish_Message = "Good Afternoon";
+		} else {
+			wish_Message = "Good Evening";
+		}
+		return new ModelAndView("wish", "message", wish_Message);
+	}
+
+}
---
+++ 7_AbstractController_inSpringWebMVC/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<jsp:forward page="wish"/>
+</body>
+</html>
