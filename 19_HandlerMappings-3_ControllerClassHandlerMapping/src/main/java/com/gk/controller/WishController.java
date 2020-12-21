da525c0a83ff749fa3fde572f76b4d355ca16240
Pranay Singh <pranay.singh.1393@gmail.com>
2020-12-21 22:11:58 +0530

    ControllerClassNameHandlerMapping is able to map the incoming request URI with the Controller class by following the below convention. a) It will take request URI value and removes extension if any extension. b) It will make the first letter as Upper Case letter in the Request URI value. c) It will append Controller to the modified Request URI value. d) It will search for the respective controller class with the above convention and sends that Controller class details to DispatcherServlet.
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ 19_HandlerMappings-3_ControllerClassHandlerMapping/src/main/java/com/gk/controller/WishController.java
@@ -0,0 +1,17 @@
+package com.gk.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class WishController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String uName = request.getParameter("uName");
+		return new ModelAndView("wish", "uname", uName);
+	}
+
+}
