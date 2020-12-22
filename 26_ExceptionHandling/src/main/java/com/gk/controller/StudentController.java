553fc04c4206c92a11b35772bd38e44ca2d280c5
Pranay Singh <pranay.singh.1393@gmail.com>
2020-12-23 02:15:54 +0530

    Exception Handling in Spring WebMvc using decalrative approach using @ExceptionHandler annotation
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

--- 26_ExceptionHandling/src/main/java/com/gk/controller/StudentController.java
+++ 26_ExceptionHandling/src/main/java/com/gk/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.gk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,11 +18,20 @@ public class StudentController {
 		return new ModelAndView("studentForm", "student", new Student());
 	}
 
+	/*
+	 * using @ExceptionHandler where you can specify one or more exceptions to be
+	 * handled. If you are specifying more than one exception then you can use comma
+	 * separated values.
+	 */
 	@RequestMapping(value = "/reg", method = RequestMethod.POST)
+	@ExceptionHandler({ StudentMarksOutOfBoundException.class })
 	public ModelAndView registration(Student student, ModelMap model) {
 		if (student.getSmarks() < 0 || student.getSmarks() > 100) {
 			throw new StudentMarksOutOfBoundException(
 					"Invalid Student Marks,Please provide student marks between 0 and 100");
+		}
+		if (!student.getSemail().endsWith("@gmail.com")) {
+			throw new StudentMarksOutOfBoundException("Invalid Email Address, It Should be of Gmail.");
 		}
 		return new ModelAndView("studentDetails", "student", student);
 	}
